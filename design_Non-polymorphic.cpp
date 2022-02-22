3d8f33a417795c407d88b0b97d0d89b849952369
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2022-02-22 13:04:48 +0530

    Add files via upload
    This algo(salary_processing_algo) will be able to process the salary of employee, and you can also see the difference b/w  polymorphic code and non-polymorphic

---
+++ design_Non-polymorphic.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+typedef enum E_TYPE {Er,Mgr,Dir} E_TYPE;
+class Engineer{
+   protected:
+    string name_; E_TYPE type_;
+   public:
+    Engineer(const string &name,E_TYPE e=Er):name_(name),type_(e){}
+    E_TYPE GetType(){return type_;}
+    void processSalary(){cout<<name_<<": process Salary for Engineer"<<endl;}
+};
+class Manager:public Engineer{
+    Engineer *reports_[10];
+   public:
+    Manager(const string &name,E_TYPE e=Mgr):Engineer(name,e){}
+    void processSalary(){cout<<name_<<": proces Salary for manager "<<endl;}
+};
+class Director:public Manager{
+   Manager *reports_[10];
+   public:
+   Director(const string &name,E_TYPE e=Dir):Manager(name,e){}
+   void processSalary(){cout<<name_<<": process salary for director"<<endl;}
+};
+
+int main(){
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    std::ios::sync_with_stdio(false);
+    //-------------------------------------------------------------------------------//
+    Engineer e1("Rohit"),e2("shuhbham"),e3("kavita"),e4("shambhu");
+    Manager  m1("kamal"),m2("rajib");Director d1("ranjana"),d2("shivam");
+    Engineer *staff[]{&e1,&e2,&e3,&e4,&m1,&m2,&d1,&d2};
+    for(int i{};i<sizeof(staff)/sizeof(Engineer*);i++){
+       E_TYPE t=staff[i]->GetType();
+       if(t==Er) staff[i]->processSalary();
+       else if(t==Mgr)((Manager*)staff[i])->processSalary();
+       else if(t==Dir)((Director*)staff[i])->processSalary();
+       else cout<<"Invalid Staff Type"<<endl;
+
+    }
+    return 0;
+
+}
